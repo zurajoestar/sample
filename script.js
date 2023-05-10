c7291e9f42a4592da61ef6aa7f6683c0db1431a8
Zura <katsuraaada@gmail.com>
2023-05-09 18:38:18 -0600

    added slideshow, futureloc.html, and footer

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
 function displaySelectedOption() {
   // Get the selected option from the dropdown
   var selectedOption = document.getElementById("options").value;
@@ -5,3 +20,4 @@ function displaySelectedOption() {
   var selectedOptionText = document.getElementById("selectedOptionText");
   selectedOptionText.innerHTML = selectedOption + " is selected";
 }
+
