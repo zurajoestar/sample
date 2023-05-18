00858283c8906be54612eb158444788c8ec7aca9
zurajoestar <katsuraaada@gmail.com>
2023-05-18 14:22:13 -0600

    added db.html, added directory "img", edited script.js

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function carousel() {
 }
 
 function displaySelectedOption() {
-  // Get the selected option from the dropdown
-  var selectedOption = document.getElementById("options").value;
-  // Set the selected option text in the main content
-  var selectedOptionText = document.getElementById("selectedOptionText");
-  selectedOptionText.innerHTML = selectedOption + " is selected";
+  location.href = "db.html";
+   // Set Item
+    var selectedOption = document.getElementById("options").value;
+    sessionStorage.setItem("lastname", selectedOption);
+
 }
 
