b5cea8fda53f162ff435c39858181771a9f765f8
zurajoestar <katsuraaada@gmail.com>
2023-05-19 01:07:44 -0600

    added maps with API key

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ function displaySelectedOption() {
   var selectedOption = document.getElementById("options").value;
   sessionStorage.setItem("lastname", selectedOption);
 }
+
+// maps credit: https://happycoding.io/tutorials/google-cloud/maps/maps-markers
+function createMap(){
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 51.127391312554295, lng: -114.20728872290304},
+    zoom: 16
+  });
+
+  const trexMarker = new google.maps.Marker({
+    position: {lat: 51.127391312554295, lng: -114.20728872290304},
+    map: map,
+    title: 'Crowfoot'
+  });
+}
