b826d3fee78888e209fcbbdf9481dfc75cb70c86
zurajoestar <katsuraaada@gmail.com>
2023-06-08 01:04:37 -0600

    changed the headers a bit, repositioned index.html

--- CrowFoot.php
+++ CrowFoot.php
@@ -31,6 +31,8 @@
                 $password = "P@ssword";
                 $dbname = "prm";
 
+                $tablename=
+
                 $conn = new mysqli($servername, $username, $password, $dbname);
                 if ($conn->connect_error) {
                         die("Connection failed: " . $conn->connect_error);
